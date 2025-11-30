11faab686412531f8541cb669acc60f0eb6c0f81
tushar75083 <tusharsawant186@gmail.com>
2025-11-30 11:20:44 +0530

    added category app and configured category model

---
+++ category/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Category
+# Register your models here.
+
+@admin.register(Category)
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['category_name','slug','description','cat_image']
