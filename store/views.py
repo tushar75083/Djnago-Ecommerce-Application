934cacf10baa2a0cef684f88a17424382c2bffed
tushar75083 <tusharsawant186@gmail.com>
2025-12-21 22:56:20 +0530

    added product search functionality

--- store/views.py
+++ store/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render,get_object_or_404
-
+from django.http import HttpResponse
 from carts.models import CartItem
 from carts.views import _cart_id
 from .models import Product
 from category.models import Category
-
+from django.db.models import Q
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
@@ -49,3 +49,19 @@ def product_detail(request, category_slug, product_slug ):
         'in_cart':in_cart,
     }
     return render(request, 'store/product_detail.html',context)
+
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter( Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+
+    context = {
+        'products':products,
+        'product_count':product_count
+    }
+
+    return render(request, 'store/store.html',context)
