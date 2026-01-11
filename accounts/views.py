44e8f1f2e79f2b4843ea1e8d72d82de4e1ddb4a1
tushar75083 <tusharsawant186@gmail.com>
2026-01-11 22:47:43 +0530

    working on checkout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+
+from carts.views import _cart_id
 from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages, auth
@@ -13,7 +15,7 @@ from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
-
+from carts.models import Cart, CartItem
 
 # Create your views here.
 
@@ -74,6 +76,19 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
+            try:
+               cart = Cart.objects.get(cart_id=_cart_id(request)) 
+               is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+               if is_cart_item_exists:
+                   cart_item = CartItem.objects.filter(cart=cart)
+
+                   for item in cart_item:
+                       item.user = user
+                       item.save()
+
+            except:
+                pass
+
             auth.login(request, user)
             messages.success(request, "You are now logged in..")
             return redirect('dashboard')
--- carts/views.py
+++ carts/views.py
@@ -4,6 +4,7 @@ from carts.models import Cart, CartItem
 from store.models import Product, Variation
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -106,8 +107,12 @@ def cart(request,total=0,quantity=0,cart_items=None):
     try:
         tax=0
         grand_total=0
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart,is_active=True)
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else: 
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart,is_active=True)
+            
         for cart_item in cart_items:
             total +=(cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
@@ -127,4 +132,29 @@ def cart(request,total=0,quantity=0,cart_items=None):
 
     return render(request,"store/cart.html", context)
 
+
+@login_required(login_url='login')
+def checkout(request,total=0,quantity=0,cart_items=None):
+    try:
+        tax=0
+        grand_total=0
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart,is_active=True)
+        for cart_item in cart_items:
+            total +=(cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+
+        tax = (2 * total)/100 # here taking 2% tax on total
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        'total' : total,
+        'quantity' : quantity,
+        'cart_items' : cart_items,
+        'tax' : tax,
+        'grand_total' : grand_total
+    }
+    return render(request, 'store/checkout.html',context)
 
