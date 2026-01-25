8db50c9a9903d302b61b8145886cc70eebd478a1
tushar75083 <tusharsawant186@gmail.com>
2026-01-25 20:10:33 +0530

    added paypal integration

--- orders/views.py
+++ orders/views.py
@@ -4,7 +4,31 @@ from carts.models import CartItem
 from orders.models import Order
 from .forms import OrderForm
 import datetime
+import json
+from .models import Payment
 # Create your views here.
+
+
+def payments(request):
+    body = json.loads(request.body)
+    order = Order.objects.get(user=request.user, is_ordered=False, order_number=body['orderID'])
+
+    #store transaction details inside payment model
+    payment = Payment(
+        user = request.user,
+        payment_id =body['transID'],
+        payment_method = body['payment_method'],
+        amount_paid = order.order_total,
+        status = body['status']
+    )
+    payment.save()
+
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+
+    return render(request, 'orders/payments.html')
+
 
 def place_order(request,total=0,quantity=0):
     current_user = request.user
@@ -50,6 +74,15 @@ def place_order(request,total=0,quantity=0):
             order_number = current_data + str(data.id)
             data.order_number = order_number
             data.save()
-            return redirect('checkout')
+
+            order = Order.objects.get(user=current_user,is_ordered=False,order_number=order_number)
+            context = {
+                'order':order,
+                'cart_items':cart_items,
+                'total':total,
+                'tax':tax,
+                'grand_total':grand_total,
+            }
+            return render(request,'orders/payments.html',context)
         else:
             return redirect('checkout')
