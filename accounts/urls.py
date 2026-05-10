06750758ee6140ea8731567ed266602c1f1def2c
tushar75083 <tusharsawant186@gmail.com>
2026-05-10 19:59:17 +0530

    handle order detail dashboard and user profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
 
     path('change_password/',views.change_password,name='change_password'),
 
+    path('order_detail/<int:order_id>/',views.order_detail,name='order_detail'),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
 from carts.views import _cart_id
-from orders.models import Order
+from orders.models import Order,OrderProduct
 from .forms import RegistrationForm,UserForm,UserProfileForm
 from .models import Account, UserProfile
 from django.contrib import messages, auth
@@ -167,8 +167,12 @@ def activate(request, uidb64, token):
 def dashboard(request):
     orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id,is_ordered=True)
     orders_count = orders.count()
+
+    userprofile=UserProfile.objects.get(user_id=request.user.id)
+
     context={
-        'orders_count':orders_count
+        'orders_count':orders_count,
+        'userprofile':userprofile,
     }
     return render(request,'accounts/dashboard.html',context)
 
@@ -302,3 +306,19 @@ def change_password(request):
             messages.error(request,"Password does not match!")
             return redirect('change_password')
     return render(request, 'accounts/change_password.html')
+
+
+@login_required(login_url='login')
+def order_detail(request,order_id):
+    order_detail = OrderProduct.objects.filter(order__order_number=order_id)
+    order=Order.objects.get(order_number=order_id)
+    subtotal = 0
+    for i in order_detail:
+        subtotal += i.product_price * i.quantity
+    context={
+        'order_detail':order_detail,
+        'order':order,
+        'subtotal':subtotal,
+    }
+
+    return render(request, 'accounts/order_detail.html',context)
